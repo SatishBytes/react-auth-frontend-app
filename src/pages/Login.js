066ae11aeb214f52f119deeabf65a93328921c8b
Satish Yadav <satishyadav42571@gmail.com>
2025-06-25 16:38:28 +0530

    Update Login.js

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -27,7 +27,7 @@ function Login() {
             return handleError('email and password are required')
         }
         try {
-            const url = 'react-auth-backend-app.vercel.app/auth/login';
+            const url = 'https://react-auth-backend-app.vercel.app/auth/login';
             const response = await fetch(url, {
                 method: "POST",
                 headers: {
